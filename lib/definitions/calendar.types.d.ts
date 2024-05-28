d451d3bc2dec1c8a1673eb81cb6265ae9c6105a3
Bularga Alexandru <bularga.alexandru@carasent.com>
2024-05-28 14:37:16 +0000

    Merged in feat/ASMA-3924-add-flags-for-event-and-task-creation (pull request #37)
    feat: ASMA-3924 Added flags on create event and task
    
    * feat: Added flags on create event and task
    
    
    Approved-by: Svyatoslav Korshakevich

--- lib/definitions/calendar.types.d.ts
+++ lib/definitions/calendar.types.d.ts
@@ -6,5 +6,7 @@ export interface ICalendarEventBus {
     delete_qnr_task_relation: {
         filter_id: string;
     };
+    on_create_task: true;
+    on_create_event: true;
 }
 //# sourceMappingURL=calendar.types.d.ts.map
--- src/definitions/calendar.types.ts
+++ src/definitions/calendar.types.ts
@@ -4,4 +4,6 @@ export interface ICalendarEventBus {
      * @param {string} filter_id can be question_id or qnr_id(query_id)
      */
     delete_qnr_task_relation: { filter_id: string }
+    on_create_task: true
+    on_create_event: true
 }
