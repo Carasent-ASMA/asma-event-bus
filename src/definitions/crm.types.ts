f979014e9db4322d3755017826cdc1810031f509
Marin Peșterean <marin.pesterean@carasent.com>
2025-10-15 15:31:03 +0000

    Merged in feat/ASMA-5178-time-registration-employees-no (pull request #63)
    feat: Added props for time tracking dialog to have details if its employee or pathfinder
    
    * feat: Added props for time tracking dialog to have details if its employee or pathfinder
    
    * 0.7.33

--- src/definitions/crm.types.ts
+++ src/definitions/crm.types.ts
@@ -2,6 +2,7 @@ export interface ICrmEventBus {
     time_tracking_dialog: {
         id?: number
         variant: 'create' | 'edit' | 'readonly'
+        is_employee_time_tracking?: boolean
     }
     nav_office_update: {
         refresh: boolean
