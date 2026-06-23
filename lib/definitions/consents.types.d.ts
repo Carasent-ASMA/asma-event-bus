8d8891314dce28764652cdf010eb9a71f597b2c8
Maxim Curudimov <Maxim.Curudimov@carasent.com>
2026-06-23 11:14:23 +0300

    feat(definitions): Add privacy_policy_tab_change to IConsentsEventBus

--- lib/definitions/consents.types.d.ts
+++ lib/definitions/consents.types.d.ts
@@ -2,5 +2,9 @@ export interface IConsentsEventBus {
     related_entities: string[];
     related_entity: string;
     on_all_mandatory_consents_accepted: boolean;
+    privacy_policy_tab_change: {
+        targetTab: string;
+        status: 'request' | 'allowed';
+    };
 }
 //# sourceMappingURL=consents.types.d.ts.map
