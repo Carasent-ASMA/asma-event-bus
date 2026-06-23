e22ed7f6e73d97e9fe3d4baef611eae58bb61346
maxim-carasent <maxim.curudimov@carasent.com>
2026-06-23 11:15:50 +0300

    ASMA-7476 admin panel privacy and consents no unsaved changes warning modal when switching tabs (#9)
    feat(definitions): Add privacy_policy_tab_change to IConsentsEventBus

--- src/definitions/consents.types.ts
+++ src/definitions/consents.types.ts
@@ -2,4 +2,5 @@ export interface IConsentsEventBus {
     related_entities: string[]
     related_entity: string
     on_all_mandatory_consents_accepted: boolean
+    privacy_policy_tab_change: { targetTab: string; status: 'request' | 'allowed' }
 }
